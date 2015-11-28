b0d0a0ec0553638082f82f1722b6b3f57f80b335
Ahmed Magdy <a.magdy@a1works.com>
2015-11-28 14:57:13 +0100

    updated the build scripts

--- repos/check_submodules_before_build.sh
+++ repos/check_submodules_before_build.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
-echo "Checking Submodules..."
-if [ $(ls -l repos/ | grep -E "^d" | grep -v modules | wc -l) -gt 0 ]
+REPOS="repos"
+MODULES_DIR=$REPOS"/modules"
+
+# create the modules directory if it does not exist
+if [ ! -d $MODULES_DIR ]; then mkdir -p $MODULES_DIR ; fi
+
+function submodulesCodeExist {
+  OUTPUT=0
+  while read SUBMODULE_DIR
+  do
+    if [ -z "$(ls $SUBMODULE_DIR)" ]; then
+      OUTPUT=1
+    fi
+  done < <(grep path .gitmodules | sed 's/.*= //')
+  return $OUTPUT
+}
+
+submodulesCodeExist
+if [ $? -eq 0 ]
 then
-  echo "Submodules sources exist."
-  if [ $(ls -l repos/modules | grep -E "^d" | wc -l) -gt 0 ]
+  if [ $(ls -l $MODULES_DIR | grep -E "^d" | wc -l) -eq 0 ]
   then
-    echo "Submodules are already built."
-  else
     echo "Building all submodules..."
     git submodule foreach ../install_submodules.sh
   fi
 else
-  echo "Submodules sources do not exist. Pulling them from Server..."
+  echo "Pulling project submodules..."
   git submodule update
-  . repos/check_submodules_before_build.sh
+  ./$REPOS/check_submodules_before_build.sh
+  echo "Built all submodules successfully."
 fi
-echo "Success"
-exit 0
 
