9064b770de405f21f39af5a728a99861131c0fb3
Ahmed Magdy <a.magdy@a1works.com>
2016-01-27 15:34:47 +0100

    Added Input merger

--- src/main/java/com/a1works/featureSelection/InputMerger.java
+++ src/main/java/com/a1works/featureSelection/InputMerger.java
@@ -1,7 +1,7 @@
 package com.a1works.featureSelection;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +24,7 @@ public class InputMerger {
         }
         List<Input> allInputs = new ArrayList<>();
         allInputs.add(input1);
-        allInputs.add(input1);
+        allInputs.add(input2);
         if (inputs != null && inputs.length > 0) {
             for (Input input : inputs) {
                 allInputs.add(input);
@@ -32,6 +32,15 @@ public class InputMerger {
         }
         MergedInput mergedInput = new MergedInput();
         for (Input input : allInputs) {
+            for (Feature feature : input.getFeatures()) {
+                mergedInput.appendFeature(feature, input.getFeatureFrequency(feature));
+            }
+            for (MlClass cls : input.getMlClasses()) {
+                mergedInput.appendMlClass(cls, input.getMlClassFrequency(cls));
+                for (Feature feature : input.getFeatures()) {
+                    mergedInput.appendFeaturePerClass(cls, feature, input.getFeatureFrequencyPerClass(feature, cls));
+                }
+            }
             mergedInput.numberOfAllRecords += input.getRecordsCount();
         }
         return mergedInput;
@@ -43,30 +52,79 @@ public class InputMerger {
         private Map<MlClass, Frequency<MlClass>> classesFrequencies = new HashMap<>();
         private long numberOfAllRecords = 0;
 
+        private void appendMlClass(MlClass cls, long frequency){
+            Frequency<MlClass> currentFrequency = classesFrequencies.get(cls);
+            if (currentFrequency == null) {
+                currentFrequency = new Frequency<>(cls);
+                classesFrequencies.put(cls, currentFrequency);
+            }
+            currentFrequency.incrementFrequency(frequency);
+        }
+
+        private void appendFeature(Feature feature, long frequency){
+            Frequency<Feature> currentFrequency = featuresFrequencies.get(feature);
+            if (currentFrequency == null) {
+                currentFrequency = new Frequency<>(feature);
+                featuresFrequencies.put(feature, currentFrequency);
+            }
+            currentFrequency.incrementFrequency(frequency);
+        }
+
+        private void appendFeaturePerClass(MlClass cls, Feature feature, long frequency){
+            Map<Feature, Frequency<Feature>> classFeaturesMap = featureFrequencyPerClass.get(cls);
+            if (classFeaturesMap == null) {
+                classFeaturesMap = new HashMap<>();
+                featureFrequencyPerClass.put(cls, classFeaturesMap);
+            }
+            Frequency<Feature> featureFreq = classFeaturesMap.get(feature);
+            if (featureFreq == null) {
+                featureFreq = new Frequency<Feature>(feature);
+                classFeaturesMap.put(feature, featureFreq);
+            }
+            featureFreq.incrementFrequency(frequency);
+        }
+
         @Override
         public Set<Feature> getFeatures() {
-            return null;
+            return Collections.unmodifiableSet(featuresFrequencies.keySet());
         }
 
         @Override
         public Set<MlClass> getMlClasses() {
-            return null;
+            return Collections.unmodifiableSet(classesFrequencies.keySet());
         }
 
         @Override
         public long getFeatureFrequency(Feature feature) {
-            return 0;
+            Frequency<Feature> freq = featuresFrequencies.get(feature);
+            if (freq == null) {
+                return 0;
+            }
+            return freq.getFrequency();
         }
 
         @Override
         public long getMlClassFrequency(MlClass cls) {
-            return 0;
+            Frequency<MlClass> freq = classesFrequencies.get(cls);
+            if (freq == null) {
+                return 0;
+            }
+            return freq.getFrequency();
         }
 
         @Override
         public long getFeatureFrequencyPerClass(Feature feature, MlClass cls) {
-            return 0;
+            Map<Feature, Frequency<Feature>> featuresFrequenciesOfClass = featureFrequencyPerClass.get(cls);
+            if (featuresFrequenciesOfClass == null) {
+                return 0;
+            }
+            Frequency<Feature> freq = featuresFrequenciesOfClass.get(feature);
+            if (freq == null) {
+                return 0;
+            }
+            return freq.getFrequency();
         }
+
 
         @Override
         public long getRecordsCount() {
@@ -75,7 +133,11 @@ public class InputMerger {
 
         @Override
         public Set<Feature> getMlClassFeatures(MlClass cls) {
-            return null;
+            Map<Feature, Frequency<Feature>> featuresFrequenciesOfClass = featureFrequencyPerClass.get(cls);
+            if (featuresFrequenciesOfClass == null) {
+                return Collections.EMPTY_SET;
+            }
+            return Collections.unmodifiableSet(featuresFrequenciesOfClass.keySet());
         }
     }
 
--- src/test/java/com/a1works/featureSelection/InputMergerTest.java
+++ src/test/java/com/a1works/featureSelection/InputMergerTest.java
@@ -2,6 +2,9 @@ package com.a1works.featureSelection;
 
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,41 +12,85 @@ import static org.junit.Assert.*;
 
 public class InputMergerTest {
 
-    private Input generateInput() {
+    private Map<Feature, Long> featuresFromArray(String[] featuresNames, long numOfRecords){
+        Map<Feature, Long> features = new HashMap<>(featuresNames.length);
+        for (String featureName : featuresNames) {
+            features.put(Feature.getInstance(featureName), numOfRecords);
+        }
+        return features;
+    }
+
+    private Map<MlClass, Long> classesFromArray(String[] classesNames, long numOfRecords){
+        Map<MlClass, Long> classes = new HashMap<>(classesNames.length);
+        for (String className : classesNames) {
+            classes.put(MlClass.getInstance(className), numOfRecords / classesNames.length);
+        }
+        return classes;
+    }
+
+    private Map<MlClass, Map<Feature, Long>> featuresPerClassFrequencies(Map<MlClass, Long> classes, Map<Feature, Long> features){
+        Map<MlClass, Map<Feature, Long>> featuresPerClass = new HashMap<>(classes.size());
+        for (MlClass cls : classes.keySet()) {
+            Map<Feature, Long> featurePerThisClass = new HashMap<>(features.size());
+            Long classFreq = classes.get(cls);
+            for (Feature feature : features.keySet()) {
+                featurePerThisClass.put(feature, classFreq);
+            }
+            featuresPerClass.put(cls, featurePerThisClass);
+        }
+        return featuresPerClass;
+    }
+
+    private long getLongValueIfExists(Long val){
+        if (val == null) {
+            return 0;
+        }
+        return val.longValue();
+    }
+
+    private Input generateInput(final String[] classesNames, final String[] featuresNames, final long numOfRecords) {
+        final Map<MlClass, Long> classes = classesFromArray(classesNames, numOfRecords);
+        final Map<Feature, Long> features = featuresFromArray(featuresNames, numOfRecords);
+        final Map<MlClass, Map<Feature, Long>> featuresPerClasses = featuresPerClassFrequencies(classes, features);
+
         Input input = new Input() {
             @Override
             public Set<Feature> getFeatures() {
-                return Collections.EMPTY_SET;
+                return Collections.unmodifiableSet(features.keySet());
             }
 
             @Override
             public Set<MlClass> getMlClasses() {
-                return Collections.EMPTY_SET;
+                return Collections.unmodifiableSet(classes.keySet());
             }
 
             @Override
             public long getFeatureFrequency(Feature feature) {
-                return 1;
+                return getLongValueIfExists(features.get(feature));
             }
 
             @Override
             public long getMlClassFrequency(MlClass cls) {
-                return 1;
+                return getLongValueIfExists(classes.get(cls));
             }
 
             @Override
             public long getFeatureFrequencyPerClass(Feature feature, MlClass cls) {
-                return 1;
+                Map<Feature, Long> featuresFrequenciesPerOneClass = featuresPerClasses.get(cls);
+                if (featuresFrequenciesPerOneClass == null) {
+                    return 0;
+                }
+                return getLongValueIfExists(featuresFrequenciesPerOneClass.get(feature));
             }
 
             @Override
             public long getRecordsCount() {
-                return 10;
+                return numOfRecords;
             }
 
             @Override
             public Set<Feature> getMlClassFeatures(MlClass cls) {
-                return Collections.EMPTY_SET;
+                return Collections.unmodifiableSet(featuresPerClasses.get(cls).keySet());
             }
         };
         return input;
@@ -54,20 +101,21 @@ public class InputMergerTest {
 
     @Before
     public void setup(){
-        input1 = generateInput();
-        input2 = generateInput();
-        input3 = generateInput();
+        input1 = generateInput(new String[]{"1", "2"}, new String[]{"a", "b", "c"}, 10);
+        input2 = generateInput(new String[]{"1", "3"}, new String[]{"a", "d", "c"}, 11);
+        input3 = generateInput(new String[]{"2", "4"}, new String[]{"a", "b", "e"}, 12);
         merger = InputMerger.getInstance();
     }
 
     @Test
     public void givenThreeInputsWhenMergedThierRecordCountsAreSummedUp(){
         Input target = merger.mergeInputs(input1, input2, input3);
-        assertEquals("When 3 input objects are merged the resulting record count must be equal to their sum", 30, target.getRecordsCount());
+        long sum = input1.getRecordsCount() + input2.getRecordsCount() + input3.getRecordsCount();
+        assertEquals("When 3 input objects are merged the resulting record count must be equal to their sum", sum, target.getRecordsCount());
     }
 
     @Test
-    public void givenThreeInputsWhenMergedThierClassFrequenciesAreSummedUp(){
+    public void givenThreeInputsWhenMergedTheirClassFrequenciesAreSummedUp(){
         MlClass cls = MlClass.getInstance("1");
         Input target = merger.mergeInputs(input1, input2, input3);
         long sum = input1.getMlClassFrequency(cls) + input2.getMlClassFrequency(cls) + input3.getMlClassFrequency(cls);
@@ -75,7 +123,7 @@ public class InputMergerTest {
     }
 
     @Test
-    public void givenThreeInputsWhenMergedThierFeatureFrequenciesAreSummedUp(){
+    public void givenThreeInputsWhenMergedTheirFeatureFrequenciesAreSummedUp(){
         Feature feature = Feature.getInstance("a");
         Input target = merger.mergeInputs(input1, input2, input3);
         long sum = input1.getFeatureFrequency(feature) + input2.getFeatureFrequency(feature) + input3.getFeatureFrequency(feature);
@@ -83,13 +131,12 @@ public class InputMergerTest {
     }
 
     @Test
-    public void givenThreeInputsWhenMergedThierFeatureFrequenciesPerClassAreSummedUp(){
+    public void givenThreeInputsWhenMergedTheirFeatureFrequenciesPerClassAreSummedUp(){
         MlClass cls = MlClass.getInstance("1");
         Feature feature = Feature.getInstance("a");
         Input target = merger.mergeInputs(input1, input2, input3);
         long sum = input1.getFeatureFrequencyPerClass(feature, cls) + input2.getFeatureFrequencyPerClass(feature, cls) + input3.getFeatureFrequencyPerClass(feature, cls);
         assertEquals("When 3 input objects are merged the resulting feature frequency per class of a certain feature and class must be equal to their sum", sum, target.getFeatureFrequencyPerClass(feature, cls));
     }
-
 }
 
